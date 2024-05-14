2c5dd463878a8f80839da6c88850624bae909bf0
OkRobotsTeam <130770843+OkRobotsTeam@users.noreply.github.com>
2024-05-14 14:40:37 -0400

    Initial commit

---
+++ src/frcradiokiosk/ResourceHandler.java
@@ -0,0 +1,81 @@
+package frcradiokiosk;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.DatagramSocket;
+import java.net.HttpURLConnection;
+
+public class ResourceHandler {
+   private static final Logger logger = LoggerFactory.getLogger(ResourceHandler.class);
+
+   public void closeReader(Reader reader) {
+      try {
+         if (reader != null) {
+            reader.close();
+            logger.debug("Reader closed");
+         }
+      } catch (IOException var3) {
+         logger.warn("Failed to close reader", var3);
+      }
+   }
+
+   public void closeWriter(Writer writer) {
+      try {
+         if (writer != null) {
+            writer.close();
+            logger.debug("Writer closed");
+         }
+      } catch (IOException var3) {
+         logger.warn("Failed to close writer", var3);
+      }
+   }
+
+   public void closeOutputStream(OutputStream out) {
+      try {
+         if (out != null) {
+            out.close();
+            logger.debug("Output stream closed");
+         }
+      } catch (IOException var3) {
+         logger.warn("Failed to close output stream", var3);
+      }
+   }
+
+   public void closeInputStream(InputStream in) {
+      try {
+         if (in != null) {
+            in.close();
+            logger.debug("Input stream closed");
+         }
+      } catch (IOException var3) {
+         logger.warn("Failed to close in stream", var3);
+      }
+   }
+
+   public void closeProcess(Process p) {
+      if (p != null) {
+         p.destroy();
+         logger.debug("Process closed");
+      }
+   }
+
+   public void closeURLConnection(HttpURLConnection conn) {
+      if (conn != null) {
+         conn.disconnect();
+         logger.debug("HTTP connection disconnected");
+      }
+   }
+
+   public void closeDatagramSocket(DatagramSocket socket) {
+      if (socket != null) {
+         if (socket.isConnected()) {
+            socket.disconnect();
+         }
+
+         socket.close();
+         logger.debug("network socket closed");
+      }
+   }
+}
